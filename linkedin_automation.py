c1a02fb2b0418603ff569c4ac8493224b224f0e2
Varun-Palleti <palletivarun@gmail.com>
2024-10-27 19:28:04 -0400

    Optimized Speed

--- linkedin_automation.py
+++ linkedin_automation.py
@@ -11,11 +11,11 @@ whatuLookinAt = os.getenv("LINKEDIN_PASSWORD")
 # Set up the Chrome driver
 chrome_driver_path = '/Users/varun/Side_Projects/companyInterest/chromedriver-mac-arm64/chromedriver'
 service = Service(executable_path=chrome_driver_path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service)  
 
 # Log in to LinkedIn
 driver.get("https://www.linkedin.com/login")
-time.sleep(2)
+time.sleep(1)
 
 # Enter login credentials
 email_input = driver.find_element(By.ID, "username")
@@ -24,7 +24,7 @@ email_input.send_keys(email)
 password_input = driver.find_element(By.ID, "password")
 password_input.send_keys(whatuLookinAt)
 password_input.send_keys(Keys.RETURN)
-time.sleep(2)  # Wait for login to complete
+time.sleep(1)  # Wait for login to complete
 successful_companies = []
 
 # Read company names from the file
@@ -37,18 +37,18 @@ for company in companies:
     try:
         # Navigate to LinkedIn search and search for the company name
         driver.get("https://www.linkedin.com/search/results/companies/")
-        time.sleep(2)  # Let the page load
+        time.sleep(1)  # Let the page load
 
         # Enter the company name in the search bar
         search_box = driver.find_element(By.XPATH, "//input[@aria-label='Search']")
         search_box.send_keys(company)
         search_box.send_keys(Keys.RETURN)
-        time.sleep(3)  # Wait for the search results to load
+        time.sleep(1)  # Wait for the search results to load
 
         # Click on the first company result
         company_link = driver.find_element(By.XPATH, "//div[contains(@class, 'entity-result')]//a")
         company_link.click()
-        time.sleep(2)  # Let the company page load
+        time.sleep(1)  # Let the company page load
 
         # Get the current company page URL
         current_url = driver.current_url
@@ -56,7 +56,7 @@ for company in companies:
         # Append '/life/' to the company URL to navigate directly to the Life tab
         life_url = current_url + "life/"
         driver.get(life_url)
-        time.sleep(2)  # Let the Life tab load
+        time.sleep(1)  # Let the Life tab load
         successful_companies.append(company)
 
         # Look for the button using the class name
